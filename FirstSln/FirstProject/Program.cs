978f4b05c183faa7a28418582c10f7761798abbe
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-04-18 13:59:40 +0400

    First commit

--- FirstSln/FirstProject/Program.cs
+++ FirstSln/FirstProject/Program.cs
@@ -11,6 +11,8 @@ namespace FirstProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World !!!");
+            Console.WriteLine("Hello World !!!");
+
         }
     }
 }
